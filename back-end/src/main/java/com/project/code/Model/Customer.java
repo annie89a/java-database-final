ff42b7007508cc756dca7ae56ea73a8e70b63769
Anisha <annie89a@gmail.com>
2025-11-16 23:00:18 -0500

    file format

--- back-end/src/main/java/com/project/code/Model/Customer.java
+++ back-end/src/main/java/com/project/code/Model/Customer.java
@@ -1,6 +1,5 @@
 package com.project.code.Model;
 
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
--- back-end/src/main/java/com/project/code/Service/OrderService.java
+++ back-end/src/main/java/com/project/code/Service/OrderService.java
@@ -1,6 +1,5 @@
 package com.project.code.Service;
 
-
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
